fe66378b9fa5d13d38e29f1a45670d90511f7c40
alan-0001 <88444410+alan-0001@users.noreply.github.com>
2021-10-08 14:15:49 -0300

    Desafio_06  Concluido
    Desenvolver um sistema que soma a nota de 3 Alunos e emita RELATORIO EXCEL CSV:
    1 - Representar o Aluno com classe Aluno (Nome, Idade, Nota)
    
    2 - Utilizar lista de alunos com ojbeto List alunos = new List ();
    
    3 - Salvar resultado em arquivos CSV para cada aluno com todos os dados e notas

---
+++ Desafio6.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using CsvHelper;
+using System.Globalization;
+
+namespace desafio6
+{
+    class Desafio6
+    {
+        static void Main(string[] args)
+        {
+            List<Aluno> aluno = new List<Aluno>();
+            aluno.Add(new Aluno { nome = "John Smith", idade = 12, nota = 7 });
+            aluno.Add(new Aluno { nome = "Ana Lee", idade = 13, nota = 9 });
+            aluno.Add(new Aluno { nome = "Maria Jane", idade = 13, nota = 8 });
+
+            foreach (var estudante in aluno)
+            {
+                Console.WriteLine((" " + estudante.nome + "\t"));
+                Console.WriteLine((" Idade: " + estudante.idade + "\t"));
+                Console.WriteLine(" Nota final: " + estudante.nota + "\t" + "\n");
+            }
+
+            int[] notas = { 7, 9, 8 };
+            int soma = notas.Sum();
+            Console.WriteLine(" A Soma das notas: " + soma);
+
+            StringBuilder csvcontent = new StringBuilder();
+
+            csvcontent.AppendLine("Alunos,Idade,Nota");
+            csvcontent.AppendLine("Ana Lee, 13,9");
+            csvcontent.AppendLine("John Smith,12,7");
+            csvcontent.AppendLine("Maria Jane, 13,8");
+            csvcontent.AppendLine("Total de notas, ," + soma);
+
+            string csvpath = @"C:\Users\Alan\source\repos\desafio6\Dados.csv";
+            File.AppendAllText(csvpath, csvcontent.ToString());
+
+            string csvpath1 = @"C:\Users\Alan\source\repos\desafio6\Ana.csv";
+            File.AppendAllText(csvpath1, csvcontent.ToString());
+
+            string csvpath2 = @"C:\Users\Alan\source\repos\desafio6\Maria.csv";
+            File.AppendAllText(csvpath2, csvcontent.ToString());
+
+            string csvpath3 = @"C:\Users\Alan\source\repos\desafio6\John.csv";
+            File.AppendAllText(csvpath3, csvcontent.ToString());
+
+
+        }
+    }
+}
